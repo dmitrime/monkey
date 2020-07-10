63efffbd07e87cd8cc4acbff16aad7424863df1e
dmitrime <dmitri807@gmail.com>
2020-07-10 19:50:04 +0800

    add infix, prefix and if/else expressions

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -17,6 +17,9 @@ func Eval(node ast.Node) object.Object {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
@@ -26,9 +29,119 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.Boolean:
 		return toBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+
 	}
 
 	return nil
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case FALSE:
+		return false
+	case TRUE:
+		return true
+	default:
+		return true
+	}
+}
+
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return toBooleanObject(left == right)
+	case operator == "!=":
+		return toBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	vleft := left.(*object.Integer).Value
+	vright := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: vleft + vright}
+	case "-":
+		return &object.Integer{Value: vleft - vright}
+	case "*":
+		return &object.Integer{Value: vleft * vright}
+	case "/":
+		return &object.Integer{Value: vleft / vright}
+	case "<":
+		return toBooleanObject(vleft < vright)
+	case ">":
+		return toBooleanObject(vleft > vright)
+	case "==":
+		return toBooleanObject(vleft == vright)
+	case "!=":
+		return toBooleanObject(vleft != vright)
+	default:
+		return NULL
+	}
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func toBooleanObject(val bool) object.Object {
