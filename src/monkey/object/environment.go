f7e6e82e8fe8111f3eb7bb87c7138e25aa0953ab
dmitrime <dmitri807@gmail.com>
2020-07-13 11:27:05 +0800

    evaluate function calls

--- src/monkey/object/environment.go
+++ src/monkey/object/environment.go
@@ -1,15 +1,28 @@
 package object
 
-func NewEnvironment() *Environment {
-	return &Environment{store: make(map[string]Object)}
-}
+// import "fmt"
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+func NewEnvironment() *Environment {
+	return &Environment{store: make(map[string]Object), outer: nil}
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		// fmt.Printf("Cannot find %s in our env, looking in outer...\n", name)
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
